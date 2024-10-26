247d22e6f7bc6fd460e4ae301344e0053421f268
Th0u305 <ahnaf4411@gmail.com>
2024-10-26 09:27:27 +0600

    fixed img not loading

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,48 +1,45 @@
-
+import './css/Button.css'
 const Footer = () => {
     return (
-        <section className="relative mt-60">
-            <div className="bg-image flex gap-5 flex-col justify-center items-center absolute bottom-[70%] left-[25%] bg-[#faf5ee] rounded-2xl w-[100vh] h-[30vh]">
-
+        <section className="relative mt-52 lg:mt-6">
+            <div id='card' className="p-4 w-[90%] xl:w-[85%] 2xl:w-[80%] md:w-[95%] bg-image bg-cover flex gap-5 flex-col justify-center items-center text-center absolute  bg-[#faf5ee] rounded-2xl h-[35vh] top-0 bottom-full left-0 right-0 m-auto">
                 <h1 className="text-2xl font-bold">Subscribe to our Newsletter</h1>
-                <p className="text-gray-400">Get the latest updates and news right in your inbox!</p>
-                <div className="flex justify-center items-center">
-                    <input type="text" placeholder="Enter Your Email" className="focus:outline-none input input-bordered w-[30em] max-w-xs" />
-                    <button className="btn bg-gradient-to-r from-purple-400 via-yellow-400 to-yellow-400 w-[10em]">Subscribe</button>
-
+                <p className="text-gray-400 w-[80%]">Get the latest updates and news right in your inbox!</p>
+                <div className="flex justify-center items-center gap-3 md:w-[80%] xl:w-[60%]">
+                    <input type="text" placeholder="Enter Your Email" className="md:h-[4em] focus:outline-none input input-bordered w-full"/>
+                    <button className="text-lg btn bg-gradient-to-r from-purple-400 via-yellow-400 to-yellow-400 md:h-16 md:w-[10em] transition-all duration-300 ease-in-out hover:scale-110">Subscribe</button>
                 </div>
-              
             </div>
 
-            <div className="bg-[#06091A] text-white flex flex-col justify-center items-center mt-28 pt-48">
-
-                <img src="assets/logo.png" alt="" />
-                <div className="flex justify-between items-center w-[90%] mb-12">
-                    <div className="w-[30%]">
-                        <h1 className="text-lg mb-3 w-fit">About Us</h1>
-                        <p className="text-gray-400 w-1/2">We are a passionate team dedicated to providing the best services to our customers.</p>
+            <div className="bg-[#06091A] text-white flex flex-col justify-center items-center mt-28 pt-48 space-y-8">
+                <img src="../assets/logo-footer.png" alt="" />
+                <div id='card3' className="flex flex-col justify-between items-center xl:items-end w-[90%] xl:w-[77%] space-y-8 xl:flex-row">
+                    <div className="flex justify-between items-top xl:w-[54%]">
+                        <div className="text-left items-center w-1/2 ">
+                            <h1 className="text-lg mb-3">About Us</h1>
+                            <p className="text-gray-400 w-[70%]">We are a passionate team dedicated to providing the best services to our customers.</p>
+                        </div>
+                    
+                        <ul className="text-right xl:text-center">
+                            <h1 className="text-lg">Quick Links</h1>
+                            <li className="text-sm text-gray-400">Home</li>
+                            <li className="text-sm text-gray-400">Service</li>
+                            <li className="text-sm text-gray-400">About</li>
+                            <li className="text-sm text-gray-400">Contact</li>
+                        </ul>
                     </div>
-                
-                    <ul className="w-auto">
-                        <h1 className="text-lg">Quick Links</h1>
-                        <li className="text-sm text-gray-400">Home</li>
-                        <li className="text-sm text-gray-400">Service</li>
-                        <li className="text-sm text-gray-400">About</li>
-                        <li className="text-sm text-gray-400">Contact</li>
-                    </ul>
 
                     <div className="text-center space-y-5">
                         <h1 className="text-lg">Subscribe</h1>
                         <p className="text-sm text-gray-400">Subscribe to our newsletter for the latest updates.</p>
-                        <div className="flex">
-                            <input type="text" placeholder="Enter Your Email" className="input input-bordered w-[30em] max-w-xs text-black" />
-                            <button className="border-none btn bg-gradient-to-r from-purple-400 via-yellow-400 to-yellow-400 w-[10em]">Subscribe</button>
+                        <div className="flex gap-2">
+                            <input type="text" placeholder="Enter Your Email" className="input input-bordered max-w-xs text-black" />
+                            <button className="border-none btn bg-gradient-to-r from-purple-400 via-yellow-400 to-yellow-400 w-[10em] transition-all duration-300 ease-in-out hover:scale-110">Subscribe</button>
                         </div>  
                     </div>
                 </div>
                 <div className="border-[1px] w-[90%] mb-5"></div>
                 <p className="text-sm text-gray-400 mb-5">@2024 Your Company All Rights Reserved.</p>
-
             </div>
         </section>
     );
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,31 @@
 import PropTypes from "prop-types";
-
 const Header = ({price, freeMoney}) => {
   
     return (
-        <section>
-            <div className="flex justify-between items-center">
-                <div className="">
-                    <img src="assets/logo.png" alt="" />
+        <section className="">
+            <div className="space-y-5 flex flex-col justify-between items-center lg:flex-row">
+                <div className="lg:w-full">
+                    <img src="../assets/logo.png" alt="" />
                 </div>
-                <div className="">
-                    <ul className="flex justify-between items-center gap-12 font-semibold">
-                        <li>Home</li>
-                        <li>Fixture</li>
-                        <li>Teams</li>
-                        <li>Schedules</li>
-                        <div className="border-2 rounded-2xl p-3 flex justify-center items-center gap-5">
-                            <p>{price}</p>
-                            <p>Coin</p>
-                            <img className="w-[30px]" src="assets/dollar.png" alt="" />
-                        </div>
-                    </ul>
+                <ul className="flex flex-row justify-between items-center gap-5 font-semibold text-xl">
+                    <li>Home</li>
+                    <li>Fixture</li>
+                    <li>Teams</li>
+                    <li>Schedules</li>
+                </ul>
+                <div className="lg:w-full flex justify-end items-center">
+                    <div className="border-2 rounded-2xl p-3 flex justify-center items-center gap-5 w-fit text-xl">
+                        <p>{price}</p>
+                        <p>Coin</p>
+                        <img className="w-[30px]" src="../assets/dollar.png" alt="" />
+                    </div>
                 </div>
             </div>
-            <div className="bg-[#000000dc] bg-image bg-no-repeat bg-cover  rounded-2xl mt-12 flex flex-col justify-center items-center gap-12 py-12">
+            <div className="bg-[#000000dc] bg-image bg-no-repeat bg-cover rounded-2xl mt-12 flex flex-col justify-center items-center text-center gap-12 py-12">
                 <img src="../assets/banner-main.png" alt="" />
                 <h1 className="text-3xl font-semibold text-white">Assemble Your Ultimate Dream 11 Cricket Team</h1>
                 <p className="text-gray-300 text-lg">Beyond Boundaries Beyond Limits</p>
-                <button onClick={()=> freeMoney()} className="btn btn-xs text-lg sm:btn-sm md:btn-md lg:btn-lg bg-active">Claim Free Credit</button>
+                <button onClick={()=> freeMoney()} className="btn btn-wide text-lg bg-active transition-all duration-300 ease-in-out hover:scale-110">Claim Free Credit</button>
             </div>
         </section>
        
