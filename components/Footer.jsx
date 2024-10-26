13f717891549543682ce45259d3450e925c4172b
Th0u305 <ahnaf4411@gmail.com>
2024-10-26 23:55:40 +0600

    added newsletter func

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,8 @@
+
 import './css/Button.css'
 import PropTypes from "prop-types";
 const Footer = ({inputButton}) => {
     return (
-        <section className="relative mt-52 lg:mt-6">
-            <div id='card' className="p-4 w-[90%] xl:w-[85%] 2xl:w-[80%] md:w-[95%] bg-image bg-cover flex gap-5 flex-col justify-center items-center text-center absolute  bg-[#faf5ee] rounded-2xl h-[35vh] top-0 bottom-full left-0 right-0 m-auto">
         <section className="relative mt-52 lg:mt-6">
             <div id='card' className="p-4 w-[90%] xl:w-[85%] 2xl:w-[80%] md:w-[95%] bg-image bg-cover flex gap-5 flex-col justify-center items-center text-center absolute  bg-[#faf5ee] rounded-2xl h-[35vh] top-0 bottom-full left-0 right-0 m-auto">
                 <h1 className="text-2xl font-bold">Subscribe to our Newsletter</h1>
@@ -20,23 +19,6 @@ const Footer = ({inputButton}) => {
                 </div>
             </div>
 
-            <div className="bg-[#06091A] text-white flex flex-col justify-center items-center mt-28 pt-48 space-y-8">
-                <img src="./assets/logo-footer.png" alt="" />
-                <div id='card3' className="flex flex-col justify-between items-center xl:items-end w-[90%] xl:w-[77%] space-y-8 xl:flex-row">
-                    <div className="flex justify-between items-top xl:w-[54%]">
-                        <div className="text-left items-center w-1/2 ">
-                            <h1 className="text-lg mb-3">About Us</h1>
-                            <p className="text-gray-400 w-[70%]">We are a passionate team dedicated to providing the best services to our customers.</p>
-                        </div>
-                    
-                        <ul className="text-right xl:text-center">
-                            <h1 className="text-lg">Quick Links</h1>
-                            <li className="text-sm text-gray-400">Home</li>
-                            <li className="text-sm text-gray-400">Service</li>
-                            <li className="text-sm text-gray-400">About</li>
-                            <li className="text-sm text-gray-400">Contact</li>
-                        </ul>
-                    </div>
             <div className="bg-[#06091A] text-white flex flex-col justify-center items-center mt-28 pt-48 space-y-8">
                 <img src="./assets/logo-footer.png" alt="" />
                 <div id='card3' className="flex flex-col justify-between items-center xl:items-end w-[90%] xl:w-[77%] space-y-8 xl:flex-row">
